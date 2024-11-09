3097e4a362dc440338ba8db5c12bae8458aba74b
Fran Camargo <francamargoads@gmail.com>
2024-11-09 20:48:05 -0300

    Update thisPersonCanVoteOrNot.js

--- Testando js no console node/thisPersonCanVoteOrNot.js
+++ Testando js no console node/thisPersonCanVoteOrNot.js
@@ -4,9 +4,9 @@ if ((age >= 18) && (age < 65)){
     console.log("This person has "+ age +" years.")
     console.log ("Compulsory vote.")
 } else if (age < 16){
-    console.log("This person has "+ age)
+    console.log("This person has "+ age +" years.")
     console.log("This person cannot vote yet.")
 } else {
-    console.log("This person has "+ age)
+    console.log("This person has " +" years.")
     console.log("Optional vote.");
 }
